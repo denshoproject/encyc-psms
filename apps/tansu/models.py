b41ba5a9b3baf4b9981a4f83c7e735f5134046e6
geoffrey jost <gjost@linux.com>
2012-03-30 17:14:04 -0700

    bolt wiki upload/update/delete functions into tansu.ImageFile

--- apps/tansu/models.py
+++ apps/tansu/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from core.models import BaseModel
-
+from tansu import wiki
 
 
 class MediaBase(BaseModel):
@@ -27,6 +27,29 @@ class ImageFile(MediaBase):
     
     def __unicode__(self):
         return self.image.name
+
+    def save(self):
+        """Uploads image to mediawiki, or updates description for existing file.
+        """
+        super(ImageFile, self).save()
+        if self.image:
+            page_name = 'File:%s' % self.image.name.capitalize()
+            response = None
+            if not wiki.exists(page_name):
+                assert False
+                response = wiki.upload_file(abspath, self.description)
+            else:
+                response = wiki.update_file(page_name, self.description)
+            assert False
+    
+    def delete(self):
+        """Deletes image from mediawiki.
+        """
+        super(ImageFile, self).delete()
+        if self.image:
+            page_name = 'File:%s' % self.image.name.capitalize()
+            response = wiki.update_file(page_name, self.description)
+            assert False
 
     def filename(self):
         return self.image.name.replace(IMAGEFILE_PATH,'')
