e8576f1582f18254a93be13d41c312ce2b60a210
geoffrey jost <gjost@linux.com>
2012-04-18 15:01:07 -0700

    update API to handle the new instance types

--- apps/tansu/api.py
+++ apps/tansu/api.py
@@ -1,11 +1,54 @@
+from django.conf.urls.defaults import url
+
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 
-from tansu.models import Entity, AudioFile, ImageFile
+from tansu.models import Entity, AudioFile, DocumentFile, ImageFile, VideoFile
+
+
+class EntityResource(ModelResource):
+    class Meta:
+        queryset = Entity.objects.all()
+        resource_name = 'entity'
+        #excludes = []
+        allowed_methods = ['get']
+        filtering = {'uri':ALL,}
+    
+    def override_urls(self):
+        """Use Densho UID in URL instead of entity.id.
+        see django-tasypie docs "Tastypie Cookbook > Using Non-PK Data For Your URLs"
+        """
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<uid>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
+
+class AudioFileResource(ModelResource):
+    class Meta:
+        queryset = AudioFile.objects.all()
+        resource_name = 'audio'
+        #excludes = []
+        allowed_methods = ['get']
+        filtering = {'uri':ALL,}
+
+class DocumentFileResource(ModelResource):
+    class Meta:
+        queryset = DocumentFile.objects.all()
+        resource_name = 'document'
+        #excludes = []
+        allowed_methods = ['get']
+        filtering = {'uri':ALL,}
 
 class ImageFileResource(ModelResource):
     class Meta:
         queryset = ImageFile.objects.all()
-        resource_name = 'imagefile'
+        resource_name = 'image'
+        #excludes = []
+        allowed_methods = ['get']
+        filtering = {'uri':ALL,}
+
+class VideoFileResource(ModelResource):
+    class Meta:
+        queryset = VideoFile.objects.all()
+        resource_name = 'image'
         #excludes = []
         allowed_methods = ['get']
         filtering = {'uri':ALL,}
--- urls.py
+++ urls.py
@@ -1,15 +1,22 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 
 from tastypie.api import Api
 
-from tansu.api import ImageFileResource
+from tansu.api import EntityResource
+from tansu.api import AudioFileResource, DocumentFileResource, ImageFileResource, VideoFileResource
 
 admin.autodiscover()
 
 v01_api = Api(api_name='v0.1')
+v01_api.register(EntityResource())
+v01_api.register(AudioFileResource())
+v01_api.register(DocumentFileResource())
 v01_api.register(ImageFileResource())
+v01_api.register(VideoFileResource())
 
 urlpatterns = patterns(
     '',
