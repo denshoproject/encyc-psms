61b491a3096d34cd474cb62e3f4aabbdeea94590
geoffrey jost <geoffrey@jostwebwerks.com>
2012-06-01 15:35:52 -0700

    updates to photo importing function, incl resizing
    refs $94, refs #95, refs #114

--- apps/sources/bin/munge.py
+++ apps/sources/bin/munge.py
@@ -23,35 +23,173 @@ problems: 9
  'en-denshopd-i236-00005-1.jpg',
  'en-denshopd-i37-00297-1.jpg'
  ]
+
+Problem keyframes:
+total_files: 92
+missing: 24
+['en-denshovh-bpeggie-01.jpg',
+ 'en-denshovh-yhisaye-01.jpg',
+ 'en-denshovh-kkara-01.jpg',
+ 'en-denshovh-tjim-01.jpg',
+ 'en-denshovh-kben_g-01.jpg',
+ 'en-denshovh-marchie-02.jpg',
+ 'en-denshovh-yfrank-01.jpg',
+ 'en-denshovh-esue-01.jpg',
+ 'en-denshovh-malfred-01.jpg',
+ 'en-denshovh-mnorman-01.jpg',
+ 'en-denshovh-uclifford-01.jpg',
+ 'en-denshovh-oruth-01.jpg',
+ 'en-denshovh-ywakako-01.jpg',
+ 'en-denshovh-kyuri-01.jpg',
+ 'en-denshovh-droger-01.jpg',
+ 'en-denshovh-hwilliam-01.jpg',
+ 'en-denshovh-trudy-02.jpg',
+ 'en-denshovh-fbob-01.jpg',
+ 'en-denshovh-ygeorge-01.jpg',
+ 'en-denshovh-wmasao-01.jpg',
+ 'en-denshovh-idaniel-01.jpg',
+ 'en-denshovh-efrank-01.jpg',
+ 'en-denshovh-hbill-01.jpg',
+ 'en-denshovh-ffrank-01.jpg',]
+problems: 1
+['en-denshopd-i226-00003-1.jpg']
+
 """
+from datetime import datetime
 import os
 import shutil
+import sys
+from PIL import Image
 from django.conf import settings
 from sources.models import Source, get_object_upload_path
 
+
+
+src_path = '/home/gjost/ps_masters/PD/en-denshopd-i226-00018-1.jpg'
+dest_dir = '/home/gjost'
+longest_side = 400
+
+def resize(src_path, dest_dir, longest_side):
+    verified = False
+    original_size = None
+    resized_size = None
+    #
+    size = longest_side,longest_side
+    src_file = os.path.basename(src_path)
+    dest_file = os.path.join(dest_dir, src_file)
+    # make dest_dir
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    # make the thumbnail
+    im = Image.open(src_path)
+    original_size = im.size
+    im.thumbnail(size, Image.ANTIALIAS)
+    try:
+        im.save(dest_file, "JPEG")
+        resized_size = im.size
+    except:
+        pass
+    # verify it
+    if os.path.exists(dest_file):
+        im1 = Image.open(dest_file)
+        if im1:
+            oversize = False
+            for dim in im1.size:
+                if dim > longest_side:
+                    oversize = True
+            if not oversize:
+                verified = dest_file
+    return verified, original_size, resized_size
+    
 def munge_photos(source_dir, name):
+    """
+    Photos
+    - thumb: 250px square (maybe try that out to begin with...?)
+    - display: 600px x-dim
+    - large: 4000px on longest dimension
+    """
+    start = datetime.now()
+    LONGEST_SIDE = 4000
     nonexist = []
+    problems = []
     total_files = 0
-    for fn in os.listdir(source_dir):
+    n = 0
+    source_files = os.listdir(source_dir)
+    num_total = len(source_files)
+    #
+    for fn in source_files:
+        n = n + 1
         total_files = total_files + 1
+        upload_path = ''
+        dest_path = ''
+        src_path = ''.join([source_dir, fn])
         eid,ext = os.path.splitext(fn)
-        print '%s %s' % (name, eid)
+        print '%s/%s %s %s' % (n, num_total, name, eid)
         #s = Source.objects.get(encyclopedia_id__contains=eid)
         try:
             s = Source.objects.get(encyclopedia_id__contains=eid)
         except:
             s = None
             nonexist.append(fn)
+        # already updated?
+        already_updated = False
         if s:
+            upload_path = get_object_upload_path(s, fn)
+            if upload_path:
+                dest_path = ''.join([settings.MEDIA_ROOT, upload_path])
+            if dest_path and os.path.exists(dest_path):
+                already_updated = True
+        if s and not already_updated:
+            # OK Go!
+            print '  >> %s' % src_path
             print '  >> %s' % s
-            path = get_object_upload_path(s, fn)
-            print '  >> %s' % (path)
+            upload_path = get_object_upload_path(s, fn)
+            dest_path = ''.join([settings.MEDIA_ROOT, upload_path])
+            print '  >> %s' % (dest_path)
+            dest_dir = os.path.dirname(dest_path)
+            print '  >> %s' % (dest_dir)
+            # copy (documents)
+            if 'pdf' in src_path:
+                if src_path and (not os.path.exists(dest_path)):
+                    dest_dir = os.path.dirname(dest_path)
+                    if not os.path.exists(dest_dir):
+                        os.makedirs(dest_dir)
+                    shutil.copy(src_path, dest_path)
+                    print '    >> COPIED'
+                if os.path.exists(dest_path) and ((not s.transcript) or (s.transcript != upload_path)):
+                    s.original = upload_path
+                    s.save()
+                    print '    SAVED'
+            else:
+                # resize
+                newfile, orig_size, new_size = resize(src_path, dest_dir, LONGEST_SIDE)
+                print '  >> it_worked: %s' % (newfile)
+                print '  >> original size: %s' % str(orig_size)
+                print '  >> new_size: %s' % str(new_size)
+                if newfile:
+                    s.original = upload_path
+                    s.save()
+                    print '    SAVED'
+                else:
+                    problems.append(fn)
         print
     print 'total_files: %s' % total_files
-    print 'problems: %s' % str(len(nonexist))
+    print 'missing: %s' % str(len(nonexist))
     print nonexist
+    print 'problems: %s' % str(len(problems))
+    print problems
+    end = datetime.now()
+    print 'start:   %s' % start
+    print 'end:     %s' % end
+    print 'elapsed: %s' % str(end - start)
+    
 
 def munge_keyframes(source_dir, name):
+    """
+    Videos (keyframes)
+    - thumb: 250px square
+    - display: 640px x - 360px y (HD), 640px x - 480px y (SD)
+    """
     nonexist = []
     total_files = 0
     for fn in os.listdir(source_dir):
@@ -86,8 +224,8 @@ def munge_transcripts(source_dir, name):
             nonexist.append(fn)
         if s:
             upload_path = get_object_upload_path(s, fn)
-            print '  >> %s' % (upload_path)
             dest_path = ''.join([settings.MEDIA_ROOT, upload_path])
+            print '  >> %s' % (upload_path)
             print '     %s' % (dest_path)
             if src_path and (not os.path.exists(dest_path)):
                 dest_dir = os.path.dirname(dest_path)
@@ -104,6 +242,6 @@ def munge_transcripts(source_dir, name):
     print 'problems: %s' % str(len(nonexist))
     print nonexist
 
-#munge_photos(     '/home/gjost/ps_masters/PD',           'pd')
-#munge_keyframes(  '/home/gjost/ps_masters/VH/keyframe',  'vhk')
-munge_transcripts('/home/gjost/ps_masters/VH/transcript','vht')
+#munge_photos(     '/home/gjost/ps_masters/PD/',           'pd')
+#munge_keyframes(  '/home/gjost/ps_masters/VH/keyframe/',  'vhk')
+#munge_transcripts('/home/gjost/ps_masters/VH/transcript/','vht')
