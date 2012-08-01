6975a57f0c08fe8d2d4503e980a98b2d6e79db3c
geoffrey jost <geoffrey@jostwebwerks.com>
2012-08-01 10:17:29 -0700

    don't emit timeline events unless published
    To publish, add "published" attrib to root tag:
        <Timeline published="1">
        ...
    refs #212, refs #215

--- apps/events/timeline.py
+++ apps/events/timeline.py
@@ -9,22 +9,23 @@ def all_events():
     xml = open('%s/events/timeline.xml' % settings.MEDIA_ROOT, 'r')
     soup = BeautifulSoup(xml, "xml")
     events = []
-    for event in soup.find_all('Event'):
-        print event
-        e = {'id': int(event.attrs['id']),
-             'start_date': event.Startdate.attrs['datenormal'],
-             'end_date': '',
-             'title': '',
-             'description': '',
-             'url': '',}
-        if hasattr(event,'Enddate') and hasattr(event.Enddate,'attrs') \
-               and event.Enddate.attrs.get('datenormal'):
-            e['end_date'] = event.Enddate.attrs['datenormal']
-        if hasattr(event,'Title') and event.Title:
-             e['title'] = event.Title.string
-        if hasattr(event,'Caption') and event.Caption:
-             e['description'] = event.Caption.string
-        if hasattr(event,'Link') and event.Link:
-             e['url'] = event.Link.string
-        events.append(e)
+    if ('published' in soup.Timeline.attrs):
+        for event in soup.find_all('Event'):
+            print event
+            e = {'id': int(event.attrs['id']),
+                 'start_date': event.Startdate.attrs['datenormal'],
+                 'end_date': '',
+                 'title': '',
+                 'description': '',
+                 'url': '',}
+            if hasattr(event,'Enddate') and hasattr(event.Enddate,'attrs') \
+                   and event.Enddate.attrs.get('datenormal'):
+                e['end_date'] = event.Enddate.attrs['datenormal']
+            if hasattr(event,'Title') and event.Title:
+                 e['title'] = event.Title.string
+            if hasattr(event,'Caption') and event.Caption:
+                 e['description'] = event.Caption.string
+            if hasattr(event,'Link') and event.Link:
+                 e['url'] = event.Link.string
+            events.append(e)
     return events
