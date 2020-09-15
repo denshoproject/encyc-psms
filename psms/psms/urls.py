denshoproject/encyc-psms

commit 9573c1a2eb78af936334e4ccfa1e559c009e450c

django2.0: Use simplified URL routing syntax

psms/psms/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponseRedirect
+from django.urls import include, path
 admin.autodiscover()
 
 from drf_yasg.views import get_schema_view
@@ -33,34 +33,34 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
     
-    url('api/auth/', include('rest_framework.urls')),
-    #path(r'^api/swagger(?P<format>\.json|\.yaml)',
+    path('api/auth/', include('rest_framework.urls')),
+    #path('api/swagger<slug:format>\.json|\.yaml)',
     #     schema_view.without_ui(cache_timeout=0), name='schema-json'
     #),
-    url(r'^api/swagger',
+    path('api/swagger/',
         schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'
     ),
-    url(r'^api/redoc',
+    path('api/redoc/',
         schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'
     ),
     
-    url(r'^api/2.0/primarysource/sitemap', sitemap, name='sources-sitemap'),
-    url(r'^api/2.0/primarysource/csv', export, name='sources-export'),
+    path('api/2.0/primarysource/sitemap/', sitemap, name='sources-sitemap'),
+    path('api/2.0/primarysource/csv/', export, name='sources-export'),
     
-    url(r'^api/2.0/sources/source/(?P<densho_id>[\w-]+)',
+    path('api/2.0/sources/source/<slug:densho_id>/',
         api.source, name='api-source'
     ),
-    url(r'^api/2.0/sources/(?P<encyclopedia_ids>[\w,-]+)',
+    path('api/2.0/sources/<slug:encyclopedia_ids>/',
         api.sources, name='api-sources'
     ),
-    url(r'^api/2.0/sources',    api.sources,    name='api-sources'),
-    url(r'^api/2.0',            api.index,      name='api-index'),
-    url(r'^api/1.0',            api.index,      name='api-index'),
-    url(r'^api',                api.index,      name='api-index'),
+    path('api/2.0/sources/',    api.sources,    name='api-sources'),
+    path('api/2.0/',            api.index,      name='api-index'),
+    path('api/1.0/',            api.index,      name='api-index'),
+    path('api/',                api.index,      name='api-index'),
     
-    url('$', index, name='sources-index'),
+    path('', index, name='sources-index'),
 ]
 # serve /media/ in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

commit 0df3d814c914307796789f02f1ea1877a9ade348

urls: Use re_path in API to get multiple Sources
Fixes problem introduced in 9573c1a2eb during the Django upgrades.

psms/psms/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponseRedirect
-from django.urls import include, path
+from django.urls import include, path, re_path
 admin.autodiscover()
 
 from drf_yasg.views import get_schema_view
@@ -52,7 +52,7 @@ urlpatterns = [
     path('api/2.0/sources/source/<slug:densho_id>/',
         api.source, name='api-source'
     ),
-    path('api/2.0/sources/<slug:encyclopedia_ids>/',
+    re_path(r'^api/2.0/sources/(?P<encyclopedia_ids>[\w,-]+)',
         api.sources, name='api-sources'
     ),
     path('api/2.0/sources/',    api.sources,    name='api-sources'),